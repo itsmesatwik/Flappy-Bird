03114931bdbe1ae82756d49f7df6eb1113829276
satwik <satwik.illini@gmail.com>
2018-05-26 19:12:55 -0500

    Changed to abstract class, added virtual functions

--- src/game_obj.h
+++ src/game_obj.h
@@ -2,21 +2,13 @@
 #include <tuple>
 
 class GameObj {
-protected:
-	ofImage obj_image_;
-	ofVec2f position_;
-	float height_;
-	float width_;
-
 public:
-	GameObj();
-	~GameObj();
-	void setDims(float width, float height);
-	void setPos(ofVec2f pos);
-	void setImage(ofImage img);
-	void update();
-	void resize(int w, int h);
-	ofImage getImage();
-	ofVec2f getPos();
-	std::tuple<int, int> getDims();
+	virtual void setDims(float width, float height) = 0;
+	virtual void setPos(ofVec2f &pos) = 0;
+	virtual void setImage(ofImage &img) = 0;
+	virtual void update() = 0;
+	virtual void resize(int w, int h) = 0;
+	virtual ofImage getImage() = 0;
+	virtual ofVec2f getPos() = 0;
+	virtual std::tuple<int, int> getDims() = 0;
 }
